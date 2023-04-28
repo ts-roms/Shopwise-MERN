96e6c60e5c5a495613b843a1770907e5555ea887
just9krish <rvamit2648@gmail.com>
2023-04-28 17:11:51 +0530

    added slider

--- client/src/components/Home/Hero/Hero.tsx
+++ client/src/components/Home/Hero/Hero.tsx
@@ -21,31 +21,29 @@ export default function Hero() {
     arrows: false,
   };
 
+  console.log(sliderRef);
+
   return (
-    <section className="relative bg-white h-[80vh] overflow-hidden">
+    <section className="relative mt-20 md:m-0 bg-white overflow-hidden">
       <div className="flex justify-between w-full absolute top-1/2 -translate-y-2/4 z-40">
         <button
-          className="bg-black rounded-tr opacity-40 rounded-br py-6 px-3 flex justify-center items-center"
+          className="bg-black rounded-tr opacity-40 rounded-br text-xl lg:text-3xl py-3 px-1.5 lg:py-6 lg:px-3 flex justify-center items-center"
           onClick={() => sliderRef?.slickPrev()}
         >
-          <FaChevronLeft color="white" size={30} />
+          <FaChevronLeft color="white" />
         </button>
         <button
-          className="bg-black rounded-tl opacity-40 rounded-bl py-6 px-3 flex justify-center items-center"
+          className="bg-black rounded-tl opacity-40 rounded-bl text-xl lg:text-3xl py-3 px-1.5 lg:py-6 lg:px-3 flex justify-center items-center"
           onClick={() => sliderRef?.slickNext()}
         >
-          <FaChevronRight color="white" size={30} />
+          <FaChevronRight color="white" />
         </button>
       </div>
       <div className="h-full w-full">
         <Slider {...settings} className="w-full h-full" ref={setSliderRef}>
           {sliderItems?.map((i) => (
             <div key={i.id}>
-              <img
-                className="h-[80vh] w-full object-cover"
-                alt=""
-                src={i.img}
-              />
+              <img className="lg:h-[80vh] w-full md:object-cover" src={i.img} />
             </div>
           ))}
         </Slider>
