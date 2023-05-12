a6d3653c287d2feac512461b852313d0f146a48e
just9krish <rvamit2648@gmail.com>
2023-05-12 15:50:22 +0530

    working on strip integration

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -11,6 +11,37 @@ const orderSchema = new mongoose.Schema(
       required: true,
     },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+
+    orderStatus: {
+      type: String,
+      default: "Processing",
+    },
+    paymentInfo: {
+      id: {
+        type: String,
+      },
+      status: {
+        type: String,
+      },
+      paymentMethod: {
+        type: String,
+      },
+    },
+
+    paidAt: {
+      type: Date,
+      default: Date.now(),
+    },
+
+    deliverdAt: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
+
+module.exports = mongoose.model("Order", orderSchema);
